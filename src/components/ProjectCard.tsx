86eb937317f1be9065621dd1891d9fd7fd6f9dc1
Andres Barilin <v.barilin@itecriocuarto.org.ar>
2022-12-22 11:00:52 -0300

    Fix projects

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,13 +1,16 @@
 // External module
 import React from 'react'
 import Image from 'next/image'
-import { FaGithub, FaGlobe} from "react-icons/fa"
+import { FaGithub, FaGlobe, FaLock} from "react-icons/fa"
 
 // Internal module
 import Style from '@styles/ProjectCard.module.scss'
 import Link from 'next/link'
 
 export default function ProjectCard({title, img, description, linkProject, linkCode}) {
+
+  const isPrivate = linkCode === 'privado'
+
   return (
     <div className={Style['project-card']}>
 
@@ -15,7 +18,13 @@ export default function ProjectCard({title, img, description, linkProject, linkC
         <Image src={img} alt={`Project ${title}`} />
         <div>
           <Link href={linkProject} target='_blank'><FaGlobe/>See Page</Link>
-          <Link href={linkCode} target='_blank'><FaGithub />Code</Link>
+          <Link
+            style={isPrivate ? {cursor: "not-allowed"} : {}}
+            onClick={(e) => isPrivate && e.preventDefault()} href={linkCode}
+            target='_blank'
+          >
+              {isPrivate ? <FaLock/> : <FaGithub />}Code {isPrivate && 'Priv'}
+          </Link>
         </div>
       </div>
 
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,13 +7,15 @@ import testGrid from 'public/images/projects/testGrid.png'
 import clon from 'public/images/projects/clon.png'
 
 const projects = [
+  // buckedup
   {
     title: 'Mi trabajo actual',
-    description: 'En mi trabajo actual estamos en el desarrollo de un Ecommerce, este es para una empresa de EE.UU. enfocada en los productos fitnes',
-    linkCode: 'https://github.com/VAB-8350/rabbit-game',
-    linkProject: 'https://vab-8350.github.io/rabbit-game/',
+    description: 'En mi trabajo actual estamos desarrollando un Ecommerce, este es para una empresa de EE.UU. enfocada en los productos fitnes',
+    linkCode: 'privado',
+    linkProject: 'https://www.buckedup.com',
     img: buckedup
   },
+  // To do
   {
     title: 'Lista de tareas',
     description: 'Este proyecto lo realice en un curso de react en platzi, tiene un disenio mas apropiado a mobile',
@@ -21,6 +23,15 @@ const projects = [
     linkProject: 'https://vab-8350.github.io/Platzi---Todos/',
     img: todo
   },
+  // Movies
+  {
+    title: 'App de peliculas',
+    description: 'Curso de platzi consumo de api rest (realizado solo con HTML CSS Y JavaScript vanilla)',
+    linkCode: 'https://github.com/VAB-8350/curso-practico-api-rest---Platzi',
+    linkProject: 'https://vab-8350.github.io/curso-practico-api-rest---Platzi/',
+    img: movies
+  },
+  // Grid
   {
     title: 'test con CSS grid',
     description: 'Curso de platzi especializado al uso de display grid',
@@ -28,13 +39,7 @@ const projects = [
     linkProject: 'https://vab-8350.github.io/Platzi---test_CSS_grid/',
     img: testGrid
   },
-  {
-    title: 'App de peliculas',
-    description: 'Curso de platzi consumo de api rest (realizado solo con HTML CSS Y JavaScript vanilla)',
-    linkCode: 'https://github.com/VAB-8350/rabbit-game',
-    linkProject: 'https://github.com/VAB-8350/curso-practico-api-rest---Platzi',
-    img: movies
-  },
+  // Rabbit
   {
     title: 'Mini juego de conejos',
     description: 'Este proyecto lo realice en un curso de animaciones en platzi',
@@ -42,10 +47,11 @@ const projects = [
     linkProject: 'https://vab-8350.github.io/rabbit-game/',
     img: rabit
   },
+  // Clone
   {
     title: 'Clon de google',
     description: 'Curso de platzi (practica de estilos)',
-    linkCode: 'https://github.com/VAB-8350/rabbit-game',
+    linkCode: 'https://github.com/VAB-8350/Platzi---Mi_clon_google',
     linkProject: 'https://vab-8350.github.io/Platzi---Mi_clon_google/',
     img: clon
   },
