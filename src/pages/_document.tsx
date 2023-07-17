f3bc434f4f0a158d9cbe57147e76d77e518c2a4c
vabklb <v.barilin@itecriocuarto.org.ar>
2023-07-17 18:28:57 -0300

    test calarity

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,8 +20,19 @@ export default function Document() {
         <meta name="description" content="Portafolio web personal, desarrollador en reactjs junior" />
         <meta name="keywords" content="Desarrollador web, React, Front-end, Portafolio, victorbarilin, Programador, Developer" />
 
+        <script type="text/javascript" 
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "i0wi5c9jyg");`
+          }}
+        />
+
         {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LZHTSH610W"></script>
+        {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-LZHTSH610W"></script>
 
         <script
           dangerouslySetInnerHTML={{
@@ -33,7 +44,7 @@ export default function Document() {
               gtag('config', 'G-LZHTSH610W');
             `
           }}
-        />
+        /> */}
       </Head>
       <body>
         <Main />
